93c4f87c1b7d62505cbee3f8d765e2b8ef34e0b3
Shankarvankudoth <153154784+Shankarvankudoth@users.noreply.github.com>
2025-05-14 18:11:24 +0530

    Update npm _install.groovy

--- vars/npm _install.groovy
+++ vars/npm _install.groovy
@@ -1,7 +1,11 @@
-stage('Install Dependencies') {
-    steps {
-        dir('backend') {
+sdef call() {
+    echo 'Installing backend dependencies...'
+    dir('backend') {
+        // Check if package.json exists before running npm install
+        if (fileExists('package.json')) {
             sh 'npm install'
+        } else {
+            error 'package.json not found in backend directory!'
         }
     }
 }
